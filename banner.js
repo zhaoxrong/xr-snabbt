5bfeef8a04483d8ea6f3829f9c04f5629db13025
xrong <zhaoxrong125@gmail.com>
2023-07-08 12:49:18 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -2,5 +2,7 @@ const pjson = require('./package.json');
 const moment = require('moment');
 const version = pjson.version
 const now = moment().format('YYYY-MM-DD');
+//const browser = require('browser');
+//const flow = 0;
 
 console.log(`/* snabbt.js Version: ${version} Build date: ${now} (c) 2015 Daniel Lundin @license MIT */`);
